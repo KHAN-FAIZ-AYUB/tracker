ed4187a3525b5ad9d6ebfb6774bae9c25bdcf1ad
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-04-12 23:48:22 +0530

    refactor code

--- server/utils/catchAsync.js
+++ server/utils/catchAsync.js
@@ -1,9 +1,6 @@
 module.exports = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((err) => {
-      // res.status(500).json({
-      //   error: "something went wrong",
-      // });
       next();
     });
   };
